9588a235bc24acc7b5de40484e3eb9eaa890a8c0
Minah Park <105715484+Minah7@users.noreply.github.com>
2023-09-18 22:47:59 +0900

    [BOJ]상근이의 여행.java
    23.09.18

---
+++ Minah/[BOJ]상근이의 여행.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int testcase = 1; testcase <= t; testcase++) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+            for(int i = 0; i < m; i++) {
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+            }
+            System.out.println(n-1);
+        }
+    }
+}
